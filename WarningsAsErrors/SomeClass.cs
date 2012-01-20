ea46c7ab7d63edf8db24180419125912ff842fd9
David Fowler <davidfowl@gmail.com>
2012-01-19 17:05:34 -0800

    Initial commit

---
+++ WarningsAsErrors/SomeClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WarningsAsErrors
+{
+    public class SomeClass
+    {
+        public string Render()
+        {
+            int x;
+            return "Hello World";
+        }
+    }
+}
